152591fe5001166d043287c3eb175474e49c83d9
JordyAaldering <aalderingjordy@gmail.com>
2020-05-30 21:09:48 +0200

    Update relax.c

--- src/relax.c
+++ src/relax.c
@@ -8,14 +8,7 @@
 #define HEAT 100.0  // heat value on the boundary
 #define EPS 0.1     // convergence criterium
 
-/**
- * allocate a vector of length "n"
- */
-double* allocVector(int n) {
-    double *v;
-    v = (double*) malloc(n * sizeof(double));
-    return v;
-}
+#define ALLOCATE(type, size) (type*)malloc(size * sizeof(type))
 
 /**
  * initialise the values of the given vector "out" of length "n"
@@ -32,8 +25,8 @@ void init(double *out, int n) {
  * computes values in vector "out" from those in vector "in"
  * assuming both are of length "n"
  */
-void relax(double *in, double *out, int n) {
-    for (int i = 1; i < n - 1; i++) {
+void relax(double *in, double *out) {
+    for (int i = 1; i < N - 1; i++) {
         out[i] = 0.25 * in[i - 1] + 0.5 * in[i] + 0.25 * in[i + 1];
     }
 }
@@ -42,28 +35,24 @@ void relax(double *in, double *out, int n) {
  * checks the convergence criterion:
  * true, iff for all indices i, we have |out[i] - in[i]| <= eps
  */
-bool isStable(double *old, double *new, int n, double eps) {
+bool isStable(double *old, double *new) {
     bool res = true;
-    for (int i = 1; i < n - 1; i++) {
-        res &= fabs(old[i] - new[i]) <= eps;
+    for (int i = 1; i < N - 1; i++) {
+        res &= fabs(old[i] - new[i]) <= EPS;
     }
 
     return res;
 }
 
 int main() {
-    int iterations = 0;
-    double *old = allocVector(N);
-    double *new = allocVector(N);
-    double *tmp;
+    double *old, *new, *tmp;
+    old = ALLOCATE(double, N);
+    new = ALLOCATE(double, N);
 
     init(old, N);
     init(new, N);
 
-    printf("size   : %d M (%d MB)\n", N / 1000000, (int) (N * sizeof(double) / (1024 * 1024)));
-    printf("heat   : %f\n", HEAT);
-    printf("epsilon: %f\n", EPS);
-
+    int iterations = 0;
     clock_t start = clock();
 
     do {
@@ -71,13 +60,13 @@ int main() {
         old = new;
         new = tmp;
 
-        relax(old, new, N);
+        relax(old, new);
         iterations++;
-    } while (!isStable(old, new, N, EPS));
+    } while (!isStable(old, new));
 
     clock_t end = clock();
-    printf("Number of iterations: %d\n", iterations);
-    printf("Done in %fs\n", (double)(end - start) / CLOCKS_PER_SEC);
+    printf("Iterations: %d\n", iterations);
+    printf("Duration: %fs\n", (double)(end - start) / CLOCKS_PER_SEC);
 
     return 0;
 }
