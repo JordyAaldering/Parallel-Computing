eec7958c42f86277c407c5be221d62f1f9020e37
JordyAaldering <aalderingjordy@gmail.com>
2020-07-16 13:32:50 +0200

    Setup project 3

---
+++ Project03/Project03.cpp
@@ -0,0 +1,25 @@
+#include <mpi.h>
+#include <stdio.h>
+
+// mpiexec -n 8 "D:\Documents\Parallel-Computing\Project03\Debug\Project03.exe"
+int main(int argc, char** argv) {
+    int rank, size;
+    double start, end;
+
+    MPI_Init(&argc, &argv);
+    start = MPI_Wtime();
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    printf("Rank %d of %d processors.\n", rank, size);
+
+    end = MPI_Wtime();
+    MPI_Finalize();
+
+    if (rank == 0) {
+        printf("Finished in %fs.\n", end - start);
+    }
+
+    return 0;
+}
